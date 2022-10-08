9bc70b963490e9bdb263c02f7bcbf2aad529dfe7
InnocentWahome <innocentwahome@gmail.com>
2022-10-08 21:46:30 +0300

    remove double footer

--- src/views/pages/Dashboard.vue
+++ src/views/pages/Dashboard.vue
@@ -1,34 +1,9 @@
 <template>
   <div>
-    <title-bar
-      v-if="userRole == 'Employee'"
-      :title-stack="titleStackEmployee"
-    />
-    <title-bar
-      v-if="userRole == 'Admin'"
-      :title-stack="titleStackAdmin"
-    />
     <hero-bar :has-right-visible="false">
       Dashboard
     </hero-bar>
     <section class="section is-main-section">
-      <notification class="is-info">
-        See what's happening on Github
-
-        <a
-          slot="right"
-          href="https://github.com/InnocentWahome/lotus-client.git"
-          target="_blank"
-          class="button is-white is-small"
-        >
-          <b-icon
-            icon="github-circle"
-            custom-size="default"
-          />
-          <span>GitHub</span>
-        </a>
-      </notification>
-
       <tiles>
         <card-widget
           class="tile is-child"
@@ -59,61 +34,26 @@
           label="Users"
         />
       </tiles>
-      <div class="columns">
-        <div class="column">
-          <card-component
-            title="System Overlook (Totals)"
-            icon="finance"
-            header-icon="reload"
-            @header-icon-click="PieChartData"
+      <div class="column">
+        <card-component
+          title="System Overlook (Totals)"
+          icon="finance"
+          header-icon="reload"
+          @header-icon-click="PieChartData"
+        >
+          <div
+            v-if="pieChartData"
+            class="chart-area mediaWahome"
           >
-            <div
-              v-if="pieChartData"
-              class="chart-area mediaWahome"
-            >
-              <pie-chart
-                :chart-data="pieChartData"
-                class=""
-                :chart-options="chartOptions"
-                :style="{ height: '50%', width: '50%' }"
-              />
-            </div>
-          </card-component>
-        </div>
-        <div class="column">
-          <card-component
-            title="Projects Performance"
-            icon="finance"
-            header-icon="reload"
-            @header-icon-click="LineChartData"
-          >
-            <div
-              v-if="lineChartData"
-              class="chart-area"
-            >
-              <line-chart
-                class=""
-                :chart-data="lineChartData"
-                :chart-options="chartOptions"
-              />
-            </div>
-          </card-component>
-        </div>
+            <pie-chart
+              :chart-data="pieChartData"
+              class=""
+              :chart-options="chartOptions"
+              :style="{ height: '30%', width: '30%' }"
+            />
+          </div>
+        </card-component>
       </div>
-      <card-component
-        title="Yearly Productivity"
-        icon="finance"
-        header-icon="reload"
-        @header-icon-click="BarChartData"
-      >
-        <div class="chart-area">
-          <bar-chart
-            :chart-data="barChartData"
-            :chart-options="chartOptions"
-            :style="{ height: '100%' }"
-          />
-        </div>
-      </card-component>
     </section>
   </div>
 </template>
@@ -121,27 +61,19 @@
 <script>
 import { defineComponent } from '@vue/composition-api'
 import * as chartConfig from '@/components/charts/chart.config.js'
-import TitleBar from '@/components/BaseTitleBar.vue'
 import HeroBar from '@/components/BaseHeroBar.vue'
 import Tiles from '@/components/BaseTiles.vue'
 import CardWidget from '@/components/BaseCardWidget.vue'
 import CardComponent from '@/components/BaseCardComponent.vue'
-import LineChart from '@/components/charts/LineChart.vue'
-import BarChart from '@/components/charts/BarChart.vue'
 import PieChart from '@/components/charts/PieChart.vue'
-import Notification from '@/components/BaseNotification.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    LineChart,
     CardComponent,
     CardWidget,
     Tiles,
     HeroBar,
-    TitleBar,
-    Notification,
-    BarChart,
     PieChart
   },
   data () {
